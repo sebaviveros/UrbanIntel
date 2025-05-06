5edbd31a159595318b6ed726f95bcb57b6bc8871
Diegotty7 <diegodiazgaete7@gmail.com>
2025-05-05 21:45:29 -0400

    Cambios menores a interfaz
    Ajustes de ngModel

--- UrbanIntelWEB/src/app/app.module.ts
+++ UrbanIntelWEB/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//  Importación del módulo para formularios
+import { FormsModule } from '@angular/forms';
+
+//  Importación del módulo para hacer peticiones HTTP (API REST)
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './views/login/login.component';
@@ -12,7 +19,9 @@ import { LoginComponent } from './views/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,         // formulario
+    HttpClientModule     // Api Rest 
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA] // Agregago de ion-icons para que sea reconocido por Angular
--- UrbanIntelWEB/src/app/views/login/login.component.ts
+++ UrbanIntelWEB/src/app/views/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,7 +10,33 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class LoginComponent {
   @ViewChild('wrapperRef') wrapperRef!: ElementRef;
 
+  // Variables conectadas a los inputs
+  email: string = '';
+  password: string = '';
+
+  constructor(private http: HttpClient) {}
+
   toggleForm() {
     this.wrapperRef.nativeElement.classList.toggle('active');
+  }
+
+  onSubmit() {
+    const loginData = {
+      email: this.email,
+      password: this.password
+    };
+
+    // Llama al backend (reemplaza la URL con la real)
+    this.http.post('localhost:4200/login', loginData)
+      .subscribe({
+        next: (response) => {
+          console.log('Login exitoso', response);
+          // Aquí puedes redirigir o guardar el token, etc.
+        },
+        error: (error) => {
+          console.error('Error de login', error);
+          // Aquí puedes mostrar un mensaje de error al usuario
+        }
+      });
   }
 }
