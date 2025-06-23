0361828a7b984941528b593acf3236d1825f8f95
Sebastian Viveros N <seb.viveros@duocuc.cl>
2025-06-22 21:10:34 -0400

    Creación auditoria
    Creación modulo auditoria

--- UrbanIntelWEB/src/app/app-routing.module.ts
+++ UrbanIntelWEB/src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './views/login/login.component'; // <-- importa el componente
+import { LoginComponent } from './views/login/login.component'; // importa el componente
+import { HomeComponent } from './views/home/home.component';
+import { LayoutComponent } from './views/layout/layout.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { QuienesSomosComponent } from './views/quienes-somos/quienes-somos.component';
+import { ManualComponent } from './views/manual/manual.component';
+import { GestionCuentasComponent } from './views/gestion-cuentas/gestion-cuentas.component';
+import { SolicitudesComponent } from './views/solicitudes/solicitudes.component';
+import { AprobacionesComponent } from './views/aprobaciones/aprobaciones.component';
+import { ReportesComponent } from './views/reportes/reportes.component';
+import { AuditoriaComponent } from './views/auditoria/auditoria.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // cuando se levanta el sistema, dirige a este path  ej: http://localhost:4200/ --> http://localhost:4200/login
-  { path: '**', redirectTo: 'login' } // ruta comodín
-  // Aquí irán las rutas como { path: 'login', component: LoginComponent }
+  { path: 'landing-page', component: LandingPageComponent},
+  {path: 'quienes-somos', component: QuienesSomosComponent},
+  {path:'manual', component: ManualComponent},
+  // rutas protegidas que usan layout (sidebar)
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'solicitudes', component: SolicitudesComponent },
+      { path: 'gestion-cuentas', component: GestionCuentasComponent },
+      { path: 'aprobaciones', component: AprobacionesComponent },
+      { path: 'reportes', component: ReportesComponent },
+      { path: 'auditoria', component: AuditoriaComponent }
+      // puedes agregar más rutas hijas aquí
+    ]
+  },
+
+  // ruta comodín para rutas no encontradas
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- UrbanIntelWEB/src/app/app.module.ts
+++ UrbanIntelWEB/src/app/app.module.ts
@@ -3,15 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
+import { LayoutComponent } from './views/layout/layout.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { QuienesSomosComponent } from './views/quienes-somos/quienes-somos.component';
+import { ManualComponent } from './views/manual/manual.component';
+
+import { DataTablesModule } from "angular-datatables";
+import { GestionCuentasComponent } from './views/gestion-cuentas/gestion-cuentas.component';
+import { SolicitudesComponent } from './views/solicitudes/solicitudes.component';
+import { AprobacionesComponent } from './views/aprobaciones/aprobaciones.component';
+import { ReportesComponent } from './views/reportes/reportes.component';
+import { AuditoriaComponent } from './views/auditoria/auditoria.component'; 
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LandingPageComponent,
+    QuienesSomosComponent,
+    ManualComponent,
+    GestionCuentasComponent,
+    SolicitudesComponent,
+    AprobacionesComponent,
+    ReportesComponent,
+    AuditoriaComponent
     // Agregarás más componentes aquí luego
   ],
-  imports: [
     BrowserModule,
     AppRoutingModule
   ],
